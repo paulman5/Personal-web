cb6521e725ae200a6e9aa5dbb4821a16eea841f0
Paul <mierlo.paul@hotmail.nl>
2024-03-08 13:41:11 +0100

    Changed typo in description front page

--- src/lib/data/articledata.tsx
+++ src/lib/data/articledata.tsx
@@ -5,7 +5,7 @@ export const Articledata = [
     date: "March 3, 2024",
     title: "Storage decentralisation, the future of digital infrastructure",
     description:
-      "Current digital infrastructure solutions still relies heavily on traditional cloud storage, which exposes them to risks such as loss of data and a cost structure dependent on service providers.",
+      "Current digital infrastructure solutions still rely heavily on traditional cloud storage, which exposes them to risks such as loss of data and a cost structure dependent on service providers.",
     author: "Paul van Mierlo",
   },
 ]
